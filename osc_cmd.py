5ec858147d7a2a342e1c062221ab594a30d8549a
Todor Trundev <trundev@gmail.com>
2020-12-25 15:28:35 +0200

    Synchronous command execution
    Generic OSC command execution with waiting for completion, i.e. the
    'inProgress' status. Added is a KEEP_RESPONSES diagnostics flag.
    
    Add an 'X-XSRF-Protected' HTML header as recommended by OSC.

--- osc_cmd.py
+++ osc_cmd.py
@@ -6,19 +6,27 @@ See:
 """
 import sys
 import json
+import time
 import pprint
 import http.client
 
 # IP address of the Insta360 ONE R WiFi AP
 OSC_HOST = '192.168.42.1'
-OSC_HEADERS = {'Accept': 'application/json'}
+OSC_HEADERS = {'Accept': 'application/json', 'X-XSRF-Protected': 1}
 INFO_URL = '/osc/info'
 STATE_URL = '/osc/state'
 CHECKFORUPDATE_URL = '/osc/checkForUpdates'
 COMMAND_EXECUTE_URL = '/osc/commands/execute'
 COMMAND_STATUS_URL = '/osc/commands/status'
 
+# Keep time-gap betweeen status polling requests
+COMMAND_POLLING_DELAY = 1
+
+# Keep each response in a file for debugging purposes
+KEEP_RESPONSES = False
+
 def send_request(conn, *args, **kwargs):
+    """Send a generic OSC request"""
     print('Request', *args)
     body = kwargs.get('body')
     # The body must be "str" or "bytes"
@@ -28,11 +36,24 @@ def send_request(conn, *args, **kwargs):
     conn.request(*args, **kwargs, headers=OSC_HEADERS)
 
     resp = conn.getresponse()
-    #print('  Responce headers:')
+    #print('  Response headers:')
     #pprint.pprint(dict(resp.headers), indent=2)
     data = resp.read()
-    print('  Responce read returned', len(data), 'bytes')
+    print('  Response read returned', len(data), 'bytes')
+    if KEEP_RESPONSES:
+        with open('%s%s.bin'%(args[0], args[1].replace('/', '-')), 'wb') as fd:
+            fd.write(data)
     return json.loads(data.decode())
+
+def run_command(conn, cmd_name, cmd_params):
+    """Send an OSC command and wait for completion"""
+    res = send_request(conn, 'POST', COMMAND_EXECUTE_URL, body={"name": cmd_name, "parameters": cmd_params})
+    while res.get('state') == 'inProgress':
+        print('  Command state "inProgress":', res.get('progress').get('completion'))
+        #pprint.pprint(res, indent=2)
+        res = send_request(conn, 'POST', COMMAND_STATUS_URL, body={'id': res['id']})
+        time.sleep(COMMAND_POLLING_DELAY)
+    return res
 
 def main(argv):
     """Main entry point"""
@@ -49,13 +70,14 @@ def main(argv):
     pprint.pprint(data, indent=2)
 
     # camera.getOptions command
-    data = send_request(conn, 'POST', COMMAND_EXECUTE_URL, body={
-                "name": "camera.getOptions",
-                "parameters": {
-                    "optionNames":
-                    ["iso", "isoSupport", "hdrSupport", "hdr", "totalSpace", "remainingSpace"]
-                }
+    data = run_command(conn, "camera.getOptions", {
+                "optionNames":
+                ["iso", "isoSupport", "hdrSupport", "hdr", "totalSpace", "remainingSpace"]
             })
+    pprint.pprint(data, indent=2)
+
+    # camera.takePicture command
+    data = run_command(conn, COMMAND_EXECUTE_URL, {"name": "camera.takePicture"})
     pprint.pprint(data, indent=2)
 
     conn.close()
